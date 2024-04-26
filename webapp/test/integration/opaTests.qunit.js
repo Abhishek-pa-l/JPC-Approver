8967975d33a57f1b908e656539507302871379be
Abhishek pal <pala@terralink-global.com>
2024-04-26 11:22:57 +0000

    new app

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/sap/jpcapproval/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comsap/jpcapproval/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
